8a848cdd58cfe8cf2ab67b4a3ba3c351915e8555
MonkaSztuder <monika.sztuder@student.put.poznan.pl>
2022-12-16 18:07:20 +0100

    BubbleTest

--- src/test/java/pl/put/poznan/sorting/logic/BubbleSortTest.java
+++ src/test/java/pl/put/poznan/sorting/logic/BubbleSortTest.java
@@ -1,7 +1,29 @@
 package pl.put.poznan.sorting.logic;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class BubbleSortTest {
+import java.util.Arrays;
 
+class BubbleSortTest {
+    private BubbleSort bubbles = new BubbleSort();
+    int[] liczby = new int[] {};
+    int[] spodziewanywynik_liczby = new int[] {};
+
+
+    @BeforeEach
+    public void setUp(){
+        liczby = new int[]{3,2,1,6,5,4};
+        spodziewanywynik_liczby = new int[] {1,2,3,4,5,6};
+    }
+
+    @Test
+    void TestBubbleSort(){
+        int[] wynik = bubbles.sortData(liczby);
+        Assertions.assertTrue(Arrays.equals(spodziewanywynik_liczby,wynik));
+    }
 }
