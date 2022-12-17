03b76fbefc72f0766e02d6cee66f5491f7366504
MonkaSztuder <monika.sztuder@student.put.poznan.pl>
2022-12-17 22:00:10 +0100

    Dok_kilka

--- src/main/java/pl/put/poznan/sorting/logic/TimeControl.java
+++ src/main/java/pl/put/poznan/sorting/logic/TimeControl.java
@@ -1,19 +1,33 @@
 package pl.put.poznan.sorting.logic;
-
+/**
+ * Time Control Function
+ */
 public class TimeControl {
-
+    /**
+     * Start of the timecount
+     */
     private static long start;
+    /**
+     * Stop of the timecount
+     */
     private static long finish;
 
-
+    /**
+     * Starts counting time
+     */
     public void startTime(){
         start = System.nanoTime();
     }
-
+    /**
+     * Stop counting time
+     */
     public void stopTime(){
         finish = System.nanoTime();
     }
-
+    /**
+     * This function returns the time of the sorting algorithm
+     * @return (finish-start) time of algorithm
+     */
     public long getTime(){
         return finish-start;
     }
