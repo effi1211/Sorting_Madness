fdf5e1ca65c7d2f0c0a3d94c9ee0ca346d3905b4
Ruscondil <kacpers2001@o2.pl>
2022-12-18 16:27:02 +0100

    poddymione zwracanie json, dziala

--- src/main/java/pl/put/poznan/sorting/rest/SortingMadnessController.java
+++ src/main/java/pl/put/poznan/sorting/rest/SortingMadnessController.java
@@ -1,10 +1,8 @@
 package pl.put.poznan.sorting.rest;
-import com.google.gson.Gson;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import  org.springframework.http.ResponseEntity;
 import pl.put.poznan.sorting.logic.SortingMadness;
@@ -22,7 +20,7 @@ public class SortingMadnessController {
     }
 
 
-    @RequestMapping(method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<SortingMadness.result> post(@RequestBody sortInfo data_in) {
         SortingMadness sorter = new SortingMadness();
         // log the parameters
@@ -30,23 +28,23 @@ public class SortingMadnessController {
         //logger.debug(Arrays.toString(transforms));
         
         //TODO dodać sprawdzanie błędów (chyba tyle tych błędów, ale niech ktoś sprawdzi plis)
-        if (dataToSort.length == 0) {
+        if (data_in.dataToSort.length == 0) {
             logger.error("Error - numbers list is empty");
             return new ResponseEntity<>("Error! Bad input, numbers list can't be empty", HttpStatus.BAD_REQUEST);
         }
-         if (algorithms.length == 0){
+         if (data_in.algorithms.length == 0){
             logger.error("Error - sorting types array is empty");
             return new ResponseEntity<>("Error! Bad input, sorting types array can't be empty", HttpStatus.BAD_REQUEST);
         }
         //TODO zwracanie wyniku i czasów
         SortingMadness.result sortedData = sorter.sort(data_in.dataToSort, data_in.iterations, data_in.desc);
-        ResponseEntity<SortingMadness.result> test = new ResponseEntity<SortingMadness.result>(sortedData, HttpStatus.OK);
+        //ResponseEntity<SortingMadness.result> test = new ResponseEntity<SortingMadness.result>(sortedData, HttpStatus.OK);
         for (String alg : data_in.algorithms) {
             //System.out.println(alg);
             //TODO no że się wykona ten algorytm co tu jest
         }
 
-        return test;
+        return new ResponseEntity<>(sortedData, HttpStatus.OK);
     }
 
 
