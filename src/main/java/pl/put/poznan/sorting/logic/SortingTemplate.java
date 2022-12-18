e0400666b856c69cce62b92e9bb5f15497a84eff
MonkaSztuder <monika.sztuder@student.put.poznan.pl>
2022-12-18 17:00:21 +0100

    SortingTemplate_Dokumentacja

--- src/main/java/pl/put/poznan/sorting/logic/SortingTemplate.java
+++ src/main/java/pl/put/poznan/sorting/logic/SortingTemplate.java
@@ -1,8 +1,21 @@
 package pl.put.poznan.sorting.logic;
-
-
+/**
+ *Sort strategy
+ */
 public interface SortingTemplate {
+    /** Number Sorting
+     *
+     * @param data_in name of data input number array
+     * @param iter maximum number of iterations
+     * @return sorted number of array
+     */
     public int[] sortData(int[] data_in, int iter);
 
+    /** String Sorting
+     *
+     * @param data_in name of the data input strings array
+     * @param iter maximum number of iterations
+     * @return sorted strings array
+     */
     //public String[] sortData(String[] data_in,int iter);
 }
