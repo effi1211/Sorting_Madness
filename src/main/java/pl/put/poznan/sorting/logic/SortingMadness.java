9c49791e556f90a85e6e35e2201b53dcf6302af9
Ruscondil <kacpers2001@o2.pl>
2022-12-19 17:59:39 +0100

    delete test code parts

--- src/main/java/pl/put/poznan/sorting/logic/SortingMadness.java
+++ src/main/java/pl/put/poznan/sorting/logic/SortingMadness.java
@@ -52,48 +52,6 @@ public class SortingMadness {
 
     }
 
-    static void printArray(int[] arr) //TODO do usunięcia na końcu
-    {
-        for (int j : arr) System.out.print(j + " ");
-        System.out.println();
-    }
-    public static void main(String[] args) //TODO do usunięcia na końcu
-    {   TimeControl timer = new TimeControl();
-        int[] arr = {64,25,12,22,11,45,76,767,56,434,43,54,65,76,78,887,566556,45,45,545454,4554,5454,54,4545,54,54,54,54,45,34,4,3,5465};
-        if(arr.length == 0){
-            System.out.println("Empty Array");
-            return;
-        }
-        String algorithm = "heap";
-        SortingTemplate sortAlgoritm = null;
-        if (Objects.equals(algorithm, "bubble")){
-            sortAlgoritm = new BubbleSort();
-        }
-        else if (Objects.equals(algorithm, "heap"))
-        {
-            sortAlgoritm = new HeapSort();
-        }
-        else if (Objects.equals(algorithm, "insert")) {
-            sortAlgoritm = new InsertionSort();
-        }
-        else if (Objects.equals(algorithm, "merge")) {
-            sortAlgoritm = new MergeSort();
-        }
-        else if (Objects.equals(algorithm, "quick")) {
-            sortAlgoritm = new QuickSort();
-        }
-        else if (Objects.equals(algorithm, "select")) {
-            sortAlgoritm = new SelectionSort();
-        }
-        timer.startTime();
-        timer.stopTime();
-
-        System.out.println("Sorted array");
-        System.out.println(timer.getTime());
-        printArray(sortAlgoritm.sortData(arr, 0));
-        printArray(reverse(sortAlgoritm.sortData(arr, 0)));
-    }
-
     static int[] reverse(int[] a)
     {   int n = a.length;
         int[] b = new int[n];
