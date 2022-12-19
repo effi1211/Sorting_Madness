d2fb0a537df89c50998697a5f663ee3de92436df
Ruscondil <kacpers2001@o2.pl>
2022-12-19 22:01:24 +0100

    insertion tests string

--- src/test/java/pl/put/poznan/sorting/logic/InsertionSortTest.java
+++ src/test/java/pl/put/poznan/sorting/logic/InsertionSortTest.java
@@ -12,12 +12,15 @@ class InsertionSortTest {
     private final InsertionSort sort = new InsertionSort();
     int[] liczby = new int[] {};
     int[] spodziewanywynik_liczby = new int[] {};
-
+    String[] znaki;
+    String[] spodziewanywynik_znaki;
 
     @BeforeEach
     public void setUp(){
         liczby = new int[]{3,2,1,6,5,4};
         spodziewanywynik_liczby = new int[] {1,2,3,4,5,6};
+        znaki = new String[]{"siema","kaczka","ząb","akwedukt","morświn?"};
+        spodziewanywynik_znaki = new String[]{"akwedukt","kaczka","morświn?","siema","ząb"};
     }
 
     @Test
@@ -30,6 +33,18 @@ class InsertionSortTest {
     void TestInsertionSort2(){
         int[] wynik = sort.sortData(liczby,1);
         assertFalse(Arrays.equals(spodziewanywynik_liczby, wynik));
+    }
+
+    @Test
+    void TestInsertionSortText1(){
+        String[] wynik = sort.sortData(znaki,0);
+        assertArrayEquals(spodziewanywynik_znaki, wynik);
+    }
+
+    @Test
+    void TestInsertionSortText2(){
+        String[] wynik = sort.sortData(znaki,1);
+        assertFalse(Arrays.equals(spodziewanywynik_znaki, wynik));
     }
 
 }
